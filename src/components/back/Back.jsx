6dfa31e842a43fde1b6b609c6529150196b36656
Bharath <sagambharath@gmail.com>
2024-05-08 23:01:38 +0530

    dd

--- src/components/back/Back.jsx
+++ src/components/back/Back.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Back = () => {
   return (
-   <div>bac</div>
+   <div> 
+    <button className="play">
+                <PlayArrow/>
+                <span>Play</span>
+             </button>
+             <button className="more">
+                <InfoOutlined/>
+                <span>info</span>
+             </button>
+
+   </div>
   )
 }
 
